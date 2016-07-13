a2daa1eb7243ab01c17d3e8efd8a1f645cce7d8d
Lei <finalfanleisy@gmail.com>
2016-07-13 15:07:24 +0000

    ex2 finished

--- ex2/costFunctionReg.m
+++ ex2/costFunctionReg.m
@@ -18,8 +18,10 @@ grad = zeros(size(theta));
 %               derivatives of the cost w.r.t. each parameter in theta
 
 
-
-
+[J, grad] = costFunction(theta, X, y);
+theta_zeroed_first = [0; theta(2:length(theta));];
+J = J + lambda / (2 * m) * sum( theta_zeroed_first .^ 2 );
+grad = grad + (lambda / m) * theta_zeroed_first;
 
 
 % =============================================================
